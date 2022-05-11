0566fc8b265508d45ead5f85f1f59e89dccd7bd8
ltl13 <66907671+ltl13@users.noreply.github.com>
2022-05-11 20:20:11 +0700

    nothing

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/ltl13/simple-todo-golang
 go 1.18
 
 require github.com/go-sql-driver/mysql v1.6.0
+
+require (
+	github.com/spacemonkeygo/openssl v0.0.0-20181017203307-c2dcc5cca94a // indirect
+	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	controller "github.com/ltl13/simple-todo-golang/controllers"
-	"log"
 	"net/http"
+
+	controller "github.com/ltl13/simple-todo-golang/controllers"
 )
 
 func main() {
@@ -13,5 +13,5 @@ func main() {
 	http.HandleFunc("/api/todo/add", todoController.AddTodo)
 	http.HandleFunc("/api/todo/update", todoController.UpdateTodoByID)
 	http.HandleFunc("/api/todo/remove", todoController.RemoveTodoByID)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
